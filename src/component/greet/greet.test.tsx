4d75175b078ec0cae7ff853b5dea0c09c7cb19b6
anhvh <anhvh@gemvietnam.com>
2022-11-15 21:54:36 +0700

    getByRole function

--- src/component/greet/greet.test.tsx
+++ src/component/greet/greet.test.tsx
@@ -1,14 +1,32 @@
-import { Greet } from "./greet"
-import {render, screen} from '@testing-library/react'
+import { Greet } from './greet';
+import { render, screen } from '@testing-library/react';
 
-test('Greet renders correctly', () => {
-    render(<Greet />)
-    const textElement = screen.getByText('Hello')
-    expect(textElement).toBeInTheDocument()
-})
+describe('Greet', () => {
+    test('Greet renders correctly', () => {
+        render(<Greet name="HA" />);
+        const textElement = screen.getByText(/Hello/);
+        expect(textElement).toBeInTheDocument();
+    });
+});
 
-test('Greet renders with a name', () => {
-    render(<Greet name='Vishwas' />)
-    const textElement = screen.getByText('Hello Vishwas')
-    expect(textElement).toBeInTheDocument()
-})
+test('Greet render Guest', () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/Guest/);
+    expect(textElement).toBeInTheDocument();
+});
+
+// describe('Nested', () => {
+//     test('Greet renders with a name', () => {
+//         render(<Greet name='Vishwas' />)
+//         const textElement = screen.getByText('Hello Vishwas')
+//         expect(textElement).toBeInTheDocument()
+//     })
+// })
+
+// test.only('Greet renders with a name', () => {
+//     render(<Greet name='Vishwas' />)
+//     const textElement = screen.getByText('Hello Vishwas')
+//     expect(textElement).toBeInTheDocument()
+// })
+// test.skip similiar to test.only
+// describe to group test, also using describe only or describe skip
